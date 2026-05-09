22be465933be37db648162a487aa0da92783bd3e
MateuszPietrzykowski17 <mateuszpietrzykowski17@gmail.com>
2026-05-09 23:29:43 +0200

    PIERWSZY COMMIT DZIALAJACY ESSA SITO POZDRO Z FONEM pozdrawiam cale zabno (dopisano dwa pliki do compile.bat)

--- compile.bat
+++ compile.bat
@@ -1,3 +1,3 @@
 @echo off
-gcc main.c fmgr_win.c fmgr_win.h -o bin/iuptest.exe -Llib/iup -Ilib/iup/include -liup -liupimglib
+gcc main.c fmgr_win.c fmgr_win.h fmgr.c fmgr.h -o bin/iuptest.exe -Llib/iup -Ilib/iup/include -liup -liupimglib
 pause
